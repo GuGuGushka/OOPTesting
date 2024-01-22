3eb3febe17a291a0b0919302499aec6646ac8699
yarik <ignatenkoyaros@yandex.ru>
2024-01-22 18:31:17 +0300

    add classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MatrrixMath matrrixMath = new MatrrixMath();
+        SimpleMath simpleMath = new SimpleMath();
+        TrigMath trigMath = new TrigMath();
+
+        System.out.println("Sum = " + simpleMath.getSumm(3,5));
+        System.out.println("Circle square = " + trigMath.getCircleSquare(32));
+        int[][] array = new int[][] {
+            {
+                4, 5, 6
+            },
+            {
+                2, 8, 11
+            },
+            {
+                0, 1, 3
+            }
+        };
+
+        System.out.println(matrrixMath.getSumMainDiagonal(array));
     }
 }
---
+++ src/SimpleMath.java
@@ -0,0 +1,9 @@
+public class SimpleMath {
+    public int getSumm(int a, int b){
+        return a + b;
+    }
+
+    public int getMul(int a, int b){
+        return a * b;
+    }
+}
